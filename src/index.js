b654b9a8cbdaee313b99a772f7c72e4effa16ee6
SHOTA KABURAKI <shota.kaburaki@gmail.com>
2019-12-21 15:41:44 +0900

    - モバイル用メニュー設定 - 右下のFloating Action Buttonをshare専用ボタンに変更

--- src/index.js
+++ src/index.js
@@ -11,12 +11,23 @@ loadFonts();
 
 $(() => {
     // Materialize componentsの初期化
-    $('.fixed-action-btn').floatingActionButton({
-      hoverEnabled: false,
-    })
+    $('.fixed-action-btn').floatingActionButton()
     $('.sidenav').sidenav()
     $('.parallax').parallax()
     $('.slider').slider()
+
+    // NOTE: Ajaxを用いてDBより一覧取得
+    // 参考: https://stackoverflow.com/questions/39883425/materialize-autocomplete-with-dynamic-data-in-jquery-ajax}}
+    $('#mobile-search').autocomplete({
+      data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://cdn.worldvectorlogo.com/logos/google-icon.svg',
+        "塩尻ワインセット": null,
+      },
+    })
+
+    $('.collapsible').collapsible()
 })
 
 // ヘッダを下スクロールで隠す処理
